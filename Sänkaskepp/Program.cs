9341b3d9c5cbfe2ce15c0b9666e106d2c1effb7b
Gustav Persson Westerberg <gustav.perssonweste@learnet.se>
2020-10-05 08:37:24 +0200

    First commit

---
+++ Sänkaskepp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sänkaskepp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
